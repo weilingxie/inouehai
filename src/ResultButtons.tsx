ddedba25a448116c821a49cf66a5d954acc64f58
Weiling Xie <sqlnever@gmail.com>
2021-05-15 23:18:41 +1200

    Create Result and ResultButtons components

--- src/ResultButtons.tsx
+++ src/ResultButtons.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-interface Props {
+interface ResultButtonProps {
     index:number,
     type:string,
     result:string,
@@ -12,36 +12,24 @@ interface Props {
         name:string,
         color:string,
     },
-    setResult(index:number,type:string,name:string): ()=>void,
-    resetResult(index:number,type:string, result:string):()=>void
+    setResult(index:number,type:string,name:string): ()=>void
 }
 
-const ResultButtons = ({index, type, result, team1, team2, setResult, resetResult} :Props)  => {
+const ResultButtons = ({index, type, result, team1, team2, setResult} :ResultButtonProps)  => {
     
-
     return (       
-        {result==='' ? 
-            (<div>
-                <button 
-                    className= {`btn btn-${team1.color==='red'?'danger':'light'}`} 
-                    onClick={()=> setResult(index,type,team1.name)}>
-                    {team1.name}
-                </button>
-                <button  
-                    className= {`btn btn-${team2.color==='red'?'danger':'light'}`} 
-                    onClick={()=> setResult(index,type,team2.name)}>
-                    {team2.name}
-                </button>
-            </div>) : 
-            (<div>
-                <span>{result} </span>
-                <button 
-                    className="btn btn-sm btn-outline-primary" 
-                    onClick={()=> resetResult(index,type, result)}>
-                    reset
-                </button>
-            </div>)
-        }           
+        <div>
+            <button 
+                className= {`btn btn-${team1.color==='red'?'danger':'light'}`} 
+                onClick={()=> setResult(index,type,team1.name)}>
+                {team1.name}
+            </button>
+            <button  
+                className= {`btn btn-${team2.color==='red'?'danger':'light'}`} 
+                onClick={()=> setResult(index,type,team2.name)}>
+                {team2.name}
+            </button>
+        </div>
     );
 }
 
